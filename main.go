44fa9aa59b42e90943c894c6a04d77cf72ddb63c
h4ndzdatm0ld <hugotinoco@icloud.com>
2022-07-31 05:45:48 +0000

    check

--- main.go
+++ main.go
@@ -18,17 +18,29 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	if len(os.Args) == 2 {
+	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
 	}
 	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else if originUnit == "F" {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
-		if shouldConvertAgain != "Y" {
+		if strings.ToUpper(shouldConvertAgain) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
